623b88fbb2a8b266ae8ccdd08ecdcb0d5d2938a6
Ranniery <ranniery.dias@live.com>
2020-10-19 16:50:17 -0300

    Adicionados comentários

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int qtdOrcs = 2;
         int id = 0;
 
+        //Roda as 2 Threads a cada presa nova
         for (int index = 1; index < presas+1; index++) {
             System.out.println("-----------------------------------------------------------");
             System.out.println("Presa " + index);
@@ -16,6 +17,7 @@ public class Main {
             OrcLock orcLock = new OrcLock();
             Thread myOrcs[] = new Thread[qtdOrcs];
 
+            //Definindo a quantidade de Orcs, foram pedidos 2 Orcs, mas preferi generalizar
             for (int i = 0; i < qtdOrcs; i++) {
                 id = i + 1;
                 myOrcs[i] = new Thread(new Orc(i, orcLock, presa));
--- src/com/company/OrcLock.java
+++ src/com/company/OrcLock.java
@@ -6,16 +6,17 @@ public class OrcLock {
     private final Lock lock = new ReentrantLock();
     int id;
 
-
     public static int Prob(double prob){
         return Math.random() < prob ? 0 : 1;
     }
 
     public void tentaMorder(int id, Presa presa) {
         boolean myLock = false;
+        //checa se ta travado ou não
         try {
             myLock = this.lock.tryLock();
         } finally {
+            //Se não estiver travado e a presa ainda não foi mordida tenta morder com 50% de chance de acerto
             if (myLock && !presa.mordida) {
                 int prob = Prob(0.5);
                 if (prob == id) {
@@ -23,11 +24,13 @@ public class OrcLock {
                     presa.mordida = true;
                     this.lock.unlock();
                 }
+                //caso não consiga morder
                 else{
                     System.out.println("O orc "+ id + " não conseguiu morder");
                     this.lock.unlock();
                 }
             }
+            //é executado se estiver travado pela outra thread
             else {
                 System.out.println("O orc "+ id + " não conseguiu morder, o outro Orc esta tentando morder");
             }
